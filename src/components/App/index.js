2940bdcceecb6e13f464523389c5b158bab7b1ce
DESKTOP-DI0HS5K\hjan1 <hjan7804@gmail.com>
2019-08-14 16:04:20 +0900

    first commit, just copy https://bit.ly/2KOHFU7 tutorial

---
+++ src/components/App/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './index.css';
+import Header from '../Header';
+import List from '../List';
+import Note from '../Note';
+import { generateId } from '../../utils/utils';
+
+class App extends React.Component{
+
+  state = {
+    notes: [
+      
+    ],
+
+    activeId: '',
+  }
+
+  handleListItemClick = (id) =>{
+    this.setState({activeId: id});
+  }
+
+  handleEditNote = (type, e) =>{
+    const notes = [...this.state.notes];
+    const note = notes.find((item)=>item.id === this.state.activeId)
+
+    note[type] = e.target.value;
+
+    this.setState({
+      notes,
+    });
+	}
+	
+	handleAddNote = () =>{
+		const id = generateId();
+		this.setState({
+			notes:[
+				...this.state.notes,
+				{
+					id,
+					title: '제목',
+					contents: '내용',
+				},
+			],
+			activeId: id,
+		});
+	}
+
+	handleDeleteNote = () => {
+		const notes = this.state.notes.filter((item) => item.id !== this.state.activeId);
+		
+		this.setState({
+			notes,
+			activeId: notes.length === 0 ? null : notes[0].id
+		})
+	}
+
+  render(){
+    const{ notes, activeId } = this.state;
+    const activeNote = notes.filter((item)=> item.id === activeId)[0];
+    return (
+      <div className="app">
+				<Header 
+					onAddNote= {this.handleAddNote}
+					onDeleteNote= {this.handleDeleteNote}
+				/>
+        <div className="container">
+          <List 
+            notes = {notes}
+            activeId = {activeId}
+            onListItemClick={this.handleListItemClick}
+          />
+          {
+            notes.length !==0 && 
+							<Note 
+								note={activeNote}
+								onEditNote={this.handleEditNote}
+							/>
+					}
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+
+class Header extends React.Component{
+    render(){
+        const {onAddNote, onDeleteNote} = this.props;
+        return(
+            <div className="header">
+                <div className="title">
+                    <span>HJ's simple notepad</span>
+                </div>
+                <div className="buttons">
+                    <button onClick={onAddNote}>추가</button>
+
+                    <button onClick={onDeleteNote}>삭제</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
